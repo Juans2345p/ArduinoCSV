cba666450866b184451cff36d0acf15f5db597ea
Juans2345p <jmejiavalbuena@gmail.com>
2024-12-22 23:20:30 -0500

    Update FileCSV.h
    I did that on with File, you don't need starting this librery

--- FileCSV.h
+++ FileCSV.h
@@ -1,54 +1,56 @@
 /*Libreria objetov de hererar de File*/
 
 
-class CSV{
+class FileCSV : public File{
   private:
-    uint8_t PinSD = 13;
+    //uint8_t PinSD = 13;
     char charX = ',';
     char charY = '\n';
-    File Arc;
   public:
+///You use when you CSV don't use , or space
+FileCSV(char DivX = ',', char DivY = '\n'){
+    charX = DivX;
+    charY = DivY;
+  }
+  FileCSV();
   //Cerrar archivo
-    CSV(uint8_t i){
+    /*FileCSV(uint8_t i){
       PinSD = i;
     }
-
+    //this isn't need because you're doing out of class
     void Close(){
-      Arc.close();
+      close();
       SD.end();
     }
-
     uint8_t Start(String Ruta){
-      /*Inicia desde 0*/
+      Inicia desde 0
       if(!SD.begin(PinSD))
       return 1;
       if(!SD.exists(Ruta))
       return 2;
       Arc = SD.open(Ruta);
       return false;
-    }
-
+    }*/
+////You use this fun when 
     String readCSV(uint8_t X, uint16_t Y){
       ///posicion
-      Arc.seek(0);
+      seek(0);
       uint16_t P = 0;
       int16_t Ca = 0;
       //movimiento en Y
       while( Y > P){
-        Arc.seek(Arc.find(charY)-2);
-        //Arc.readStringUntil(charY);
+        seek(find(charY)-2);
         P++;
       }
       ///Moviendo en X
       P = 0;
       while( X > P){
-        Arc.seek(Arc.find(charX)-2);
-        //Arc.readStringUntil(charX);
+        seek(find(charX)-2);
         P++;
       }
       String Cache = "";
       while(1){
-        Ca = Arc.read();
+        Ca = read();
         if(Ca == -1)
         return Cache;
         else if(Ca == charX)
@@ -60,5 +62,3 @@ class CSV{
       }
     }
 };
-
-
